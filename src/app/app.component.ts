2110034427a4cec6b3736a2cb191e7718b6f3127
Mahid Ahmad <mahid@workstory.io>
2023-08-23 04:15:57 -0400

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'teamapp';
+  newMemberName: string = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  add() {
+    if (!this.newMemberName.length) {
+      this.errorMessage = "Name can't be empty";
+      return;
+    }
+
+    this.errorMessage = '';
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+
+    console.log(this.members);
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  generate() {
+    throw new Error('Method not implemented.');
+  }
 }
